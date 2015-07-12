cf57a5b7e4306e212c55e8b83232524c318eb2ab
deyin <deyinx.chen@gmail.com>
2015-07-12 11:31:54 +0800

    refactor

--- src/com/cloudaward/IconfontDownloader.java
+++ src/com/cloudaward/IconfontDownloader.java
@@ -9,13 +9,8 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * °¢ÀïÂèÂèÍ¼±ê¿âÏÂÔØÆ÷
@@ -25,170 +20,111 @@ import java.util.concurrent.Executors;
  */
 public class IconfontDownloader {
 
-  public static String colors[] = new String[] {"00bb9c", "11cd6e", "56abe4", "9d55b8", "33475f",
-      "f4c600", "ea8010", "eb4f38", "ecf0f1", "a9b7b7", "000000"};
+	public static String urlPrefix = "http://www.iconfont.cn/uploads/fonts/font-";
+	public static String urlSuffix = "-.png";
+	public static String colorArgs = "color=";
+	public static String sizeArgs = "size=";
 
-  public static Map<String, String> sizeNameMap = new HashMap<String, String>();
+	public static String outputDir = "iconfont-download";
 
-  static {
-    sizeNameMap.put("16", "drawable-ldpi");
-    sizeNameMap.put("32", "drawable-mdpi");
-    sizeNameMap.put("64", "drawable-hdpi");
-    sizeNameMap.put("128", "drawable-xhdpi");
-  }
+	public static Map<String, String> sizeNameMap = new HashMap<String, String>();
 
-  public static String urlPrefix = "http://www.iconfont.cn/uploads/fonts/font-";
-  public static String urlSuffix = "-.png";
-  public static String colorArgs = "color=";
-  public static String sizeArgs = "size=";
+	static {
+		sizeNameMap.put("32", "drawable-mdpi");
+		sizeNameMap.put("48", "drawable-hdpi");
+		sizeNameMap.put("64", "drawable-xhdpi");
+		sizeNameMap.put("96", "drawable-xxhdpi");
+	}
 
-  public static String baseDir = "iconfont-extracted";
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.out.println("Input argument error, Usage: java -jar IconfontDownloader.jar $id $color [$saveFileName]");
+			return;
+		}
+		long begin = System.nanoTime();
+		System.out.println("Download starting...");
+		System.out.println("Download directory is " + new File(outputDir).getAbsolutePath());
+		if (args.length == 3) {
+			download(args[0], args[1], args[2]);
+		}else {
+			download(args[0], args[1], null);
+		}
+		System.out.println("Download end, total spent is " + (System.nanoTime() - begin) / 1.0e9 + " second(s)");
+	}
 
-  private static int threshold = 10; // default
+	public static void download(String id, String color, String outputFileName) {
+		for (String size : sizeNameMap.keySet()) {
+			final String strUrl = urlPrefix + id + urlSuffix + "?" + colorArgs
+					+ color + "&" + sizeArgs + size;
+			String diffSize = sizeNameMap.get(size);
+			File dir = new File(outputDir + File.separator + "id_" + id + File.separator + "color_#" + color
+					+ File.separator + diffSize);
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
+			if (outputFileName == null) {
+				outputFileName = "ic_" + id + ".png";
+			}
+			download(strUrl, new File(dir, outputFileName));
+			System.out.println("size " + size + " x " + size + " OK!");
+		}
+	}
 
-  public static void main(String[] args) {
-    long begin = System.nanoTime();
-    System.out.println("Start to download...");
-    if (args.length < 1) {
-      System.err.println("Error usage!");
-      System.out.println("Usage: Extractor $idList");
-      System.out.println("Usage: Extractor $startId $endId [threshold]");
-    } else if (args.length == 1) {
-      String[] split = args[0].split(",");
-      for (String str : split) {
-        int id = Integer.parseInt(str);
-        download(id);
-      }
-    } else if (args.length > 1) {
-      int start = Integer.parseInt(args[0]);
-      if (start < 0) {
-        throw new IllegalArgumentException("StartId value must be positive number");
-      }
-      int end = Integer.parseInt(args[1]);
-      if (end < 0) {
-        throw new IllegalArgumentException("EndId value must be positive number");
-      }
-      if (args.length == 3) {
-        threshold = Integer.parseInt(args[2]);
-        if (threshold < 0) {
-          throw new IllegalArgumentException("Threshold value must be positive number");
-        }
-      }
-      int count = end - start;
-      if (count < 0) {
-        throw new IllegalArgumentException("EndId value must greater than StartId");
-      }
-      if (count < threshold) { // sequence
-        System.out.println("Count < " + threshold + ", using sequence download!");
-        sequenceDownload(start, end);
-      } else { // concurrent
-        System.out.println("Count = " + count + " > " + threshold + ", using concurrent downlaod!");
-        concurrentDownoad(start, end);
-      }
-    }
-    System.out.println("End to download...");
-    System.out
-        .println("Download total spent " + (System.nanoTime() - begin) / 1.0e9 + " second(s)");
-  }
-
-  private static void concurrentDownoad(int start, int end) {
-    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    List<Callable<Void>> tasks = new ArrayList<Callable<Void>>();
-    for (int i = start; i <= end; i++) {
-      final int id = i;
-      tasks.add(new Callable<Void>() {
-        @Override
-        public Void call() throws Exception {
-          download(id);
-          return null;
-        }
-      });
-    }
-    try {
-      pool.invokeAll(tasks);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } finally {
-      pool.shutdown();
-    }
-  }
-
-  private static void sequenceDownload(int start, int end) {
-    for (int i = start; i <= end; i++) {
-      download(i);
-    }
-  }
-
-  public static void download(int id) {
-    for (String color : colors) {
-      for (String size : sizeNameMap.keySet()) {
-        File dir = new File(baseDir + "\\" + "color_" + color, sizeNameMap.get(size));
-        if (!dir.exists()) {
-          dir.mkdirs();
-        }
-        String url = urlPrefix + id + urlSuffix + "?" + colorArgs + color + "&" + sizeArgs + size;
-        download(url, new File(dir, "ic_" + id + ".png"));
-      }
-    }
-    System.out.println("Downlod id = " + id + " image OK!");
-  }
-
-  public static void download(String strUrl, File file) {
-    HttpURLConnection conn = null;
-    InputStream in = null;
-    ByteArrayOutputStream out = null;
-    FileOutputStream fos = null;
-    try {
-      URL url = new URL(strUrl);
-      conn = (HttpURLConnection) url.openConnection();
-      conn.setRequestMethod("GET");
-      conn.setConnectTimeout(60000);
-      int responseCode = conn.getResponseCode();
-      if (responseCode != 200) {
-        System.err.println(responseCode + " not ok, url = " + strUrl);
-        return;
-      }
-      in = new BufferedInputStream(url.openStream());
-      out = new ByteArrayOutputStream();
-      byte[] buf = new byte[1024];
-      int n = 0;
-      while (-1 != (n = in.read(buf))) {
-        out.write(buf, 0, n);
-      }
-      byte[] response = out.toByteArray();
-      fos = new FileOutputStream(file);
-      fos.write(response);
-      fos.flush();
-    } catch (MalformedURLException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      if (in != null) {
-        try {
-          in.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-      if (fos != null) {
-        try {
-          fos.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-      if (out != null) {
-        try {
-          out.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-      if (conn != null) {
-        conn.disconnect();
-      }
-    }
-  }
-
+	public static void download(String strUrl, File file) {
+		HttpURLConnection conn = null;
+		InputStream in = null;
+		ByteArrayOutputStream out = null;
+		FileOutputStream fos = null;
+		try {
+			URL url = new URL(strUrl);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setConnectTimeout(60000);
+			int responseCode = conn.getResponseCode();
+			if (responseCode != 200) {
+				System.err.println(responseCode + " error, url = " + strUrl);
+				return;
+			}
+			in = new BufferedInputStream(url.openStream());
+			out = new ByteArrayOutputStream();
+			byte[] buf = new byte[1024];
+			int n = 0;
+			while (-1 != (n = in.read(buf))) {
+				out.write(buf, 0, n);
+			}
+			byte[] response = out.toByteArray();
+			fos = new FileOutputStream(file);
+			fos.write(response);
+			fos.flush();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (out != null) {
+				try {
+					out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				conn.disconnect();
+			}
+		}
+	}
 }
